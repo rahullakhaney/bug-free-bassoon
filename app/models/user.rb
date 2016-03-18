262d43a2051fc65b55b38c99214aa1979e59e0c4
rahullakhaney <rahul@codebrew.io>
2016-03-18 23:04:22 +0530

    Added Accounts Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-        def devise_confirmable?
+         validate :email_is_unique, on: :create
+         after_create :create_account
+
+
+        def confirmation_required?
             false
+        end
+
+        private
+
+        def email_is_unique
+          return false unless self.errors[:email].empty?
+
+          unless Account.find_by_email(email).nil?
+            errors.add(:email, " is already used by another account")
+          end
+        end
+
+        def create_account
+          account = Account.new(:email => email)
+          account.save!
         end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318164131) do
+ActiveRecord::Schema.define(version: 20160318172328) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
