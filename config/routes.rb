190203ab2be8ea7b0725c15ab181f65c6bdc1454
rahullakhaney <rahul@codebrew.io>
2016-03-20 12:29:41 +0530

    Removed recaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "users/registrations"}
+  devise_for :users
   get 'pages/home'
   root 'pages#home'
   get 'components/home' => 'components#home'
